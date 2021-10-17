25d5a306ba83e994a936f76aae38a4d190ca014c
marunemo <chys3697@gmail.com>
2021-10-18 03:18:59 +0900

    feat: styled menu form

--- public/menu.js
+++ public/menu.js
@@ -1,3 +1,4 @@
+const haksikIndex = ['Korean Table - 아침', 'Korean Table - 점심', 'Korean Table - 저녁', 'Fry Fry', 'Noodle Road', 'Hao', 'Grace Garden', 'Mix Rice']
 var haksik = [];
 var moms = [];
 
@@ -10,6 +11,17 @@ $.getJSON('./secureKey.json', (secureData) => {
 });
 
 function addItems(menuList) {
-    for (const menu of menuList)
-        $(".menuTable").append(`<tr><td>${menu['menu_kor']}</td></tr>`);
+    menuList.forEach((menu, i) => {
+        if (!menu['menu_kor'].includes('-운영없음-')) {
+            const haksikList = menu['menu_kor'].replace('-원산지: 메뉴게시판 참조-', '').trim();
+            if (i < 3)
+                $(".menuTable").append(`<tr><td class="haksikTitle">${haksikIndex[i]}</td><td class="haksikList">${haksikList}</td></tr>`);
+            else {
+                const list = haksikList.split('\n');
+                for (const item of list) {
+                    $(".menuTable").append(`<tr><td class="haksikTitle">${haksikIndex[i]}</td><td class="haksikList">${item}</td></tr>`);
+                }
+            }
+        }
+    });
 }
