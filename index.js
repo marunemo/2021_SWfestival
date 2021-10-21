f1d1d7c6911b845b9cc0e242c760b24307750cbe
marunemo <chys3697@gmail.com>
2021-10-22 01:08:16 +0900

    feat: change port to http

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.static('public'));
 getApiJson();
 
-app.listen(3000, () => console.log('porting on 3000'));
+app.listen(80, () => console.log('porting on http(80)'));
