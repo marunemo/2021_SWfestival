367289bf9d0a3793361944ef4ab59f1d79ea4a11
marunemo <chys3697@gmail.com>
2021-10-21 20:15:22 +0900

    feat: refactoring

--- api/parsing.js
+++ api/parsing.js
@@ -3,74 +3,74 @@ import fs from 'fs';
 // const fs = require('fs');
 
 const haksikIndex = [
-    'Korean Table',
-    'Fry Fry',
-    'Noodle Road',
-    'Hao',
-    'Grace Garden',
-    'Mix Rice',
-    'Moms Kitchen'
+  'Korean Table',
+  'Fry Fry',
+  'Noodle Road',
+  'Hao',
+  'Grace Garden',
+  'Mix Rice',
+  'Moms Kitchen'
 ];
 const dailyIndex = [
-    'morning',
-    'lunch',
-    'dinner'
+  'morning',
+  'lunch',
+  'dinner'
 ];
 
 function fetchMenu(menuUri) {
-    axios.request({
-        method: 'GET',
-        url: menuUri,
-        headers: {'Content-Type': 'application/json'},
-    }).then((response) => {
-        const menuData = response.data;
-        let haksik = menuData['haksik'];
-        let moms = menuData['moms'];
-        let kotae = haksik.slice(0, 3);
-        haksik = haksik.slice(3, haksik.length);
+  axios.request({
+    method: 'GET',
+    url: menuUri,
+    headers: { 'Content-Type': 'application/json' },
+  }).then((response) => {
+    const menuData = response.data;
+    let haksik = menuData['haksik'];
+    let moms = menuData['moms'];
+    let kotae = haksik.slice(0, 3);
+    haksik = haksik.slice(3, haksik.length);
 
-        const parseKotae = parseMenu(haksikIndex[0], kotae, true);
-        const parseHaksik = parseMenu(haksikIndex.slice(1, 6), haksik);
-        const parseMoms = parseMenu(haksikIndex[6], moms, true);
-        exportJson('hakgwanMenu.json', Object.assign(parseKotae, parseHaksik));
-        exportJson('momsMenu.json', parseMoms);
-    });
+    const parseKotae = parseMenu(haksikIndex[0], kotae, true);
+    const parseHaksik = parseMenu(haksikIndex.slice(1, 6), haksik);
+    const parseMoms = parseMenu(haksikIndex[6], moms, true);
+    exportJson('hakgwanMenu.json', Object.assign(parseKotae, parseHaksik));
+    exportJson('momsMenu.json', parseMoms);
+  });
 }
 
 
 function parseMenu(id, menuList, isDaily = false) {
-    let menuJson = {};
-    if (isDaily) {
-        menuJson[id] = { dailyMeal: isDaily };
-        for (const index in dailyIndex) {
-            const menuKor = menuList[index]['menu_kor'];
-            if (!menuKor.includes('-운영없음-')) {
-                const menuStr = menuKor.replace('-원산지: 메뉴게시판 참조-', '').trim();
-                const haksikList = menuStr.split('\r\n');
-                menuJson[id][dailyIndex[index]] = { menu: haksikList };
-            }
-        }
-    } else {
-        for (const index in menuList) {
-            menuJson[id[index]] = { dailyMeal: isDaily };
-            const menuKor = menuList[index]['menu_kor'];
-            if (menuKor.includes('-운영없음-'))
-                menuJson[id[index]].menu = [];
-            else {
-                const menuStr = menuKor.replace('-원산지: 메뉴게시판 참조-', '').trim();
-                const haksikList = menuStr.split('\r\n');
-                menuJson[id[index]].menu = haksikList;
-            }
-        }
+  let menuJson = {};
+  if (isDaily) {
+    menuJson[id] = { dailyMeal: isDaily };
+    for (const index in dailyIndex) {
+      const menuKor = menuList[index]['menu_kor'];
+      if (!menuKor.includes('-운영없음-')) {
+        const menuStr = menuKor.replace('-원산지: 메뉴게시판 참조-', '').trim();
+        const haksikList = menuStr.split('\r\n');
+        menuJson[id][dailyIndex[index]] = { menu: haksikList };
+      }
     }
-    return menuJson;
+  } else {
+    for (const index in menuList) {
+      menuJson[id[index]] = { dailyMeal: isDaily };
+      const menuKor = menuList[index]['menu_kor'];
+      if (menuKor.includes('-운영없음-'))
+        menuJson[id[index]].menu = [];
+      else {
+        const menuStr = menuKor.replace('-원산지: 메뉴게시판 참조-', '').trim();
+        const haksikList = menuStr.split('\r\n');
+        menuJson[id[index]].menu = haksikList;
+      }
+    }
+  }
+  return menuJson;
 }
 
 function exportJson(fileName, jsonData) {
-    fs.writeFileSync('./public/resource/' + fileName, JSON.stringify(jsonData), 'utf-8');
+  fs.writeFileSync('./public/resource/' + fileName, JSON.stringify(jsonData), 'utf-8');
 }
 
 export function getApiJson() {
-    const secureData = JSON.parse(fs.readFileSync('./api/secureKey.json', 'utf-8'));
-    fetchMenu(secureData.haksikMenu);
+  const secureData = JSON.parse(fs.readFileSync('./api/secureKey.json', 'utf-8'));
+  fetchMenu(secureData.haksikMenu);
 }
--- public/menu.js
+++ public/menu.js
@@ -1,32 +1,38 @@
 
 const haksikIndex = [
-    'Korean Table',
-    'Fry Fry',
-    'Noodle Road',
-    'Hao',
-    'Grace Garden',
-    'Mix Rice'
+	'Korean Table',
+	'Fry Fry',
+	'Noodle Road',
+	'Hao',
+	'Grace Garden',
+	'Mix Rice'
 ];
 
 $.getJSON('./resource/hakgwanMenu.json', menu => {
-    for (const index of haksikIndex)
-        addItems('haksik', index, menu[index]);
+	for (const index of haksikIndex)
+		addItems('haksik', index, menu[index]);
 })
 $.getJSON('./resource/momsMenu.json', menu => addItems('moms', '맘스 키친', menu['Moms Kitchen']))
 $.getJSON('./resource/loungeMenu.json', menu => addItems('lounge', '한동 라운지', menu['Handong Lounge']))
 $.getJSON('./resource/graceMenu.json', menu => addItems('grace', '더 그레이스 테이블', menu['The Grace Table']))
 
 function addItems(id, restName, menuList) {
-    if (menuList['dailyMeal']) {
-        menuList['morning']['menu'].forEach((menu) => appendMenu(id, restName + ' - 아침', menu));
-        menuList['lunch']['menu'].forEach((menu) => appendMenu(id, restName + ' - 점심', menu));
-        menuList['dinner']['menu'].forEach((menu) => appendMenu(id, restName + ' - 저녁', menu));
-    }
-    else {
-        appendMenu(id, restName, menuList['menu'])
-    }
+	if (menuList['dailyMeal']) {
+		if (restName === 'Korean Table') {
+			appendMenu(id, restName + ' - 아침', menuList['morning']['menu']);
+			appendMenu(id, restName + ' - 점심', menuList['lunch']['menu']);
+			appendMenu(id, restName + ' - 저녁', menuList['dinner']['menu']);
+		} else {
+			menuList['morning']['menu'].forEach((menu) => appendMenu(id, restName + ' - 아침', menu));
+			menuList['lunch']['menu'].forEach((menu) => appendMenu(id, restName + ' - 점심', menu));
+			menuList['dinner']['menu'].forEach((menu) => appendMenu(id, restName + ' - 저녁', menu));
+		}
+	}
+	else {
+		menuList['menu'].forEach(menu => appendMenu(id, restName, menuList['menu']));
+	}
 }
 
 function appendMenu(id, title, menu) {
-    $(`#${id}`).append(`<tr><td class="haksikTitle">${title}</td><td class="haksikList">${menu}</td></tr>`);
+	$(`#${id}`).append(`<tr><td class="haksikTitle">${title}</td><td class="haksikList">${menu}</td></tr>`);
 }
