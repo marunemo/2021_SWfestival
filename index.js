2a13c3967668104c1f1bb91972e81559b8d2ecf0
marunemo <chys3697@gmail.com>
2021-10-23 04:50:38 +0900

    feat: set fetch timer

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import { getApiJson } from './api/parsing.js';
 const app = express();
 
 app.use(express.static('public'));
-getApiJson();
+setInterval(getApiJson, 30 * 60 * 1000);
 
 app.listen(8080, () => console.log('porting on 8080'));
